229cf78567751ffef97084e232a363154dabe360
Xizuki <64124274+Xizuki@users.noreply.github.com>
2024-01-21 10:09:59 +0800

    graphic settings

--- Assets/OptionsManager.cs
+++ Assets/OptionsManager.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.Rendering;
+using TMPro;
 
 public class OptionsManager : MonoBehaviour
 {
@@ -14,10 +15,40 @@ public class OptionsManager : MonoBehaviour
     private int prevDifficulty;
 
     public float obstacleDestroyDistance;
+
+    public int currentGraphics;
+
+
+    public Slider graphicsSlider;
+    public Slider soundSlider;
+    public Slider musicSlider;
+    public Slider difficultySlider;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        LoadPlayerPrefs();
         XizukiMethods.GameObjects.Xi_Helper_GameObjects.MonoInitialization<OptionsManager>(ref instance, this);
+    }
+
+    public void LoadPlayerPrefs()
+    {
+        currentGraphics = PlayerPrefs.GetInt("Quality", 3);
+        soundVolume = PlayerPrefs.GetFloat("Sound", 0.5f);
+        musicVolume = PlayerPrefs.GetFloat("Music", 0.5f);
+        difficulty = PlayerPrefs.GetInt("Difficulty", 1);
+
+
+        graphicsSlider.value = currentGraphics;
+        soundSlider.value = soundVolume;
+        musicSlider.value = musicVolume;
+        difficultySlider.value = difficulty;
+
+
+        QualitySettings.SetQualityLevel(currentGraphics);
+
+
     }
 
     // Update is called once per frame
@@ -26,13 +57,32 @@ public class OptionsManager : MonoBehaviour
 
     }
 
+    public void GraphicsSlider(Slider slider)
+    {
+        currentGraphics = (int)slider.value;
+        QualitySettings.SetQualityLevel(currentGraphics);
+        PlayerPrefs.SetInt("Quality", currentGraphics);
+
+    }
+
+    [ContextMenu("Change Graphics")]
+    public void ChangeGraphics()
+    {
+        //GraphicsSettings.defaultRenderPipeline = graphicSettingsRenderPipeLines[currentGraphics];
+        QualitySettings.SetQualityLevel(currentGraphics);
+    }
+
     public void SoundSlider(Slider slider)
     {
         soundVolume = slider.value;
+        PlayerPrefs.SetFloat("Sound", soundVolume);
+
     }
     public void MusicSlider(Slider slider)
     {
         musicVolume = slider.value;
+        PlayerPrefs.SetFloat("Music", musicVolume);
+
     }
 
     public void ChangeDifficulty(Slider slider)
@@ -48,10 +98,13 @@ public class OptionsManager : MonoBehaviour
 
         prevDifficulty = difficulty;
 
+        PlayerPrefs.SetInt("Difficulty", difficulty);
+
         GameManager.instance.RemoveObstaclesNearPlayer();
+
     }
 
- 
+
 
     public void ExitGame()
     {
